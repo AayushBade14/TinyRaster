6fbafb417a346d4b2fb5f19d4c8d95dfd5198122
AayushBade14 <aayushrajubade@gmail.com>
2025-12-22 16:43:31 +0530

    Modified Framebuffer.h/Math.h : Added method for swap and moved Interpolation methods to Math.h | Added Vec2 support to rasterization operations in framebuffer class.

--- Framebuffer.h
+++ Framebuffer.h
@@ -226,13 +226,13 @@ public:
 
   void PutPixel(const Vec2& v, CP color)
   {
-    if(x < 0 || x >= m_iWidth || y < 0 || y >= m_iHeight) return;
+    if(v.iX() < 0 || v.iX() >= m_iWidth || v.iY() < 0 || v.iY() >= m_iHeight) return;
 
-    int index = y * m_iWidth + x;
+    int index = v.iY() * m_iWidth + v.iX();
     m_pPixels[index].SetColor(color);
     
     #ifdef DEBUG
-    std::cout << "Pixel put into framebuffer at: (" << x << ", " << y << ")" << std::endl;
+    std::cout << "Pixel put into framebuffer at: (" << v.iX() << ", " << v.iY() << ")" << std::endl;
     #endif
   }
 
@@ -247,7 +247,19 @@ public:
     std::cout << "Pixel put into framebuffer at: (" << x << ", " << y << ")" << std::endl;
     #endif  
   }
-  
+ 
+  void PutPixel(const Vec2& v, uint8_t r, uint8_t g, uint8_t b)
+  {
+    if(v.iX() < 0 || v.iX() >= m_iWidth || v.iY() < 0 || v.iY() >= m_iHeight) return;
+
+    int index = v.iY() * m_iWidth + v.iX();
+    m_pPixels[index].SetColor(r, g, b);
+    
+    #ifdef DEBUG
+    std::cout << "Pixel put into framebuffer at: (" << v.iX() << ", " << v.iY() << ")" << std::endl;
+    #endif
+  }
+
   void PutLine(float x0, float y0, float x1, float y1, CP color)
   {
     
@@ -295,6 +307,48 @@ public:
       for(int y = (int)y0; y < (int)y1; y++)
       {
         PutPixel((int)xs[(int)(y - y0)], (int)y, color);
+      }
+    }
+    
+    #ifdef DEBUG
+    std::cout << "Rendered a line: (" << x0 << ", " << y0 << ") -> (" << x1 << ", " << y1 << ")"
+      << std::endl;
+    #endif
+  }
+
+  void PutLine(const Vec2& p0, const Vec2& p1, CP color)
+  {
+    
+    if(std::fabs(p1.X() - p0.X()) > std::abs(p1.Y() - p0.Y()))
+    {
+      //line is horizontal-ish
+      //making sure x0 < x1
+      if(p0.X() > p1.X())
+      {
+        Swap(p0, p1);
+      }
+
+      std::vector<float> ys = Interpolate(p0, p1);
+
+      for(int x = p0.iX(); x < p1.iX(); x++)
+      {
+        PutPixel(x, (int)ys[x - p0.iX()], color);
+      }
+    }
+    else
+    {
+      //line is vertical-ish
+      //making sure y0 < y1
+      if(p0.Y() > p1.Y())
+      {
+        Swap(p0, p1);
+      }
+
+      std::vector<float> xs = Interpolate(p0, p1);
+
+      for(int y = p0.iY(); y < p1.iY(); y++)
+      {
+        PutPixel((int)xs[y - p0.iY()], y, color);
       }
     }
     
@@ -352,25 +406,99 @@ public:
     }
 
     #ifdef DEBUG
-    std::cout << "Rendered a line: (" << x0 << ", " << y0 << ") -> (" << x1 << ", " << y1 << ")"
-      << std::endl;
+    std::cout << "Rendered a line: " << p0 << " -> " << p1 << std::endl;
     #endif
   }
-  
+ 
+void PutLine(const Vec2& p0, const Vec2& p1, uint8_t r, uint8_t g, uint8_t b)
+  {
+    
+    if(std::fabs(p1.X() - p0.X()) > std::abs(p1.Y() - p0.Y()))
+    {
+      //line is horizontal-ish
+      //making sure x0 < x1
+      if(p0.X() > p1.X())
+      {
+        Swap(p0, p1);
+      }
+
+      std::vector<float> ys = Interpolate(p0, p1);
+
+      for(int x = p0.iX(); x < p1.iX(); x++)
+      {
+        PutPixel(x, (int)ys[x - p0.iX()], r, g, b);
+      }
+    }
+    else
+    {
+      //line is vertical-ish
+      //making sure y0 < y1
+      if(p0.Y() > p1.Y())
+      {
+        Swap(p0, p1);
+      }
+
+      std::vector<float> xs = Interpolate(p0, p1);
+
+      for(int y = p0.iY(); y < p1.iY(); y++)
+      {
+        PutPixel((int)xs[y - p0.iY()], y, r, g, b);
+      }
+    }
+    
+    #ifdef DEBUG
+    std::cout << "Rendered a line: " << p0 << " -> " << p1 << std::endl;
+    #endif
+  }
+
   void PutWireframeTriangle(float x0, float y0, float x1, float y1, float x2, float y2, CP color)
   {
     PutLine(x0, y0, x1, y1, color);
     PutLine(x1, y1, x2, y2, color);
     PutLine(x0, y0, x2, y2, color);
+
+    #ifdef DEBUG
+    std::cout << "Rendered a wireframe-triangle: " << Vec2(x0, y0) << ", " << Vec2(x1, y1) <<
+    ", " << Vec2(x2, y2) << std::endl;
+    #endif
   }
-  
+ 
+  void PutWireframeTriangle(const Vec2& p0, const Vec2& p1, const Vec2& p2, CP color)
+  {
+    PutLine(p0, p1, color);
+    PutLine(p1, p2, color);
+    PutLine(p0, p2, color);
+    
+    #ifdef DEBUG
+    std::cout << "Rendered a wireframe-triangle: " << p0 << ", " << p1 <<
+    ", " << p2 << std::endl;
+    #endif
+  }
+
   void PutWireframeTriangle(float x0, float y0, float x1, float y1, float x2, float y2, uint8_t r, uint8_t g, uint8_t b)
   {
     PutLine(x0, y0, x1, y1, r, g, b);
     PutLine(x1, y1, x2, y2, r, g, b);
     PutLine(x0, y0, x2, y2, r, g, b);
+    
+    #ifdef DEBUG
+    std::cout << "Rendered a wireframe-triangle: " << Vec2(x0, y0) << ", " << Vec2(x1, y1) <<
+    ", " << Vec2(x2, y2) << std::endl;
+    #endif
   }
   
+  void PutWireframeTriangle(const Vec2& p0, const Vec2& p1, const Vec2& p2, uint8_t r, uint8_t g, uint8_t b)
+  {
+    PutLine(p0, p1, r, g, b);
+    PutLine(p1, p2, r, g, b);
+    PutLine(p0, p2, r, g, b);
+  
+    #ifdef DEBUG
+    std::cout << "Rendered a wireframe-triangle: " << p0 << ", " << p1 <<
+    ", " << p2 << std::endl;
+    #endif
+  }
+
   void PutFilledTriangle(float x0, float y0, float x1, float y1, float x2, float y2, CP color)
   {
     if(y1 < y0)
@@ -435,8 +563,66 @@ public:
         PutPixel(x, y, color);
       }
     }
+
+    #ifdef DEBUG
+    std::cout << "Rendered a filled-triangle: " << Vec2(x0, y0) << ", " << Vec2(x1, y1) <<
+    ", " << Vec2(x2, y2) << std::endl;
+    #endif
   }
- 
+
+  void PutFilledTriangle(const Vec2& p0, const Vec2& p1, const Vec2& p2, CP color)
+  {
+    if(p1.Y() < p0.Y())
+    {
+      Swap(p0, p1);
+    }
+
+    if(p2.Y() < p0.Y())
+    {
+      Swap(p0, p2);
+    }
+
+    if(p2.Y() < p1.Y())
+    {
+      Swap(p1, p2);
+    }
+
+    std::vector<float> x01 = Interpolate_(p0, p1);
+    std::vector<float> x12 = Interpolate_(p1, p2);
+    std::vector<float> x02 = Interpolate_(p0, p2);
+    
+    x01.pop_back();
+    x01.insert(x01.end(), x12.begin(), x12.end());
+    std::vector<float> x012 = x01;
+
+    std::vector<float> x_left;
+    std::vector<float> x_right;
+    int m = std::floor(x012.size()/2);
+    if(x02[m] < x012[m])
+    {
+      x_left = x02;
+      x_right = x012;
+    }
+    else
+    {
+      x_left = x012;
+      x_right = x02;
+    }
+
+    for(int y = (int)std::ceil(p0.Y()); y < (int)std::ceil(p2.Y()); y++)
+    {
+      for(int x = (int)x_left[y - p0.iY()]; x < (int)x_right[y - p0.iY()]; x++)
+      {
+        PutPixel(x, y, color);
+      }
+    }
+
+    #ifdef DEBUG
+    std::cout << "Rendered a filled-triangle: " << p0 << ", " << p1 <<
+    ", " << p2 << std::endl;
+    #endif
+  }
+
   void PutFilledTriangle(float x0, float y0, float x1, float y1, float x2, float y2, uint8_t r, uint8_t g, uint8_t b)
   {
     if(y1 < y0)
@@ -501,6 +687,52 @@ public:
         PutPixel(x, y, r, g, b);
       }
     }
+    
+    #ifdef DEBUG
+    std::cout << "Rendered a filled-triangle: " << Vec2(x0, y0) << ", " << Vec2(x1, y1) << ", "
+    << Vec2(x2, y2) << std::endl;
+    #endif
+  }
+
+  void PutFilledTriangle(const Vec2& p0, const Vec2& p1, const Vec2& p2, uint8_t r, uint8_t g, uint8_t b)
+  {
+    if(p1.Y() < p0.Y()){ Swap(p0, p1); }
+    if(p2.Y() < p0.Y()){ Swap(p0, p2); }
+    if(p2.Y() < p1.Y()){ Swap(p1, p2); }
+
+    std::vector<float> x01 = Interpolate_(p0, p1);
+    std::vector<float> x12 = Interpolate_(p1, p2);
+    std::vector<float> x02 = Interpolate_(p0, p2);
+    
+    x01.pop_back();
+    x01.insert(x01.end(), x12.begin(), x12.end());
+    std::vector<float> x012 = x01;
+
+    std::vector<float> x_left;
+    std::vector<float> x_right;
+    int m = std::floor(x012.size()/2);
+    if(x02[m] < x012[m])
+    {
+      x_left = x02;
+      x_right = x012;
+    }
+    else
+    {
+      x_left = x012;
+      x_right = x02;
+    }
+
+    for(int y = (int)std::ceil(p0.Y()); y < (int)std::ceil(p2.Y()); y++)
+    {
+      for(int x = (int)x_left[y - p0.iY()]; x < (int)x_right[y - p0.iY()]; x++)
+      {
+        PutPixel(x, y, r, g, b);
+      }
+    }
+
+    #ifdef DEBUG
+    std::cout << "Rendered a filled-triangle: " << p0 << ", " << p1 << ", " << p2 << std::endl;
+    #endif
   }
 
   void BlitFramebuffer()
@@ -532,29 +764,6 @@ private:
   
   Color* m_pPixels;
 
-  std::vector<float> Interpolate(float i0, float d0, float i1, float d1)
-  {
-    float epsilon = 0.0001f;
-
-    if(abs(i0 - i1) <= epsilon)
-    {
-      return {d0};
-    }
-    
-    std::vector<float> values;
-
-    float a = (d1 - d0)/(i1 - i0);
-
-    float d = d0;
-
-    for(int i = (int)i0; i <= (int)i1; i++)
-    {
-      values.push_back(d);
-      d = d + a;
-    }
-
-    return values;
-  }
 };
 
 #endif
--- Math.h
+++ Math.h
@@ -12,6 +12,7 @@
 //--------------------------------------------------------------------
 #include <cmath>
 #include <iostream>
+#include <vector>
 
 class Vec2
 {
@@ -52,44 +53,44 @@ public:
   //on the heap memory.
   
   //getters and setters
-  float X(){ return m_fX; }
-  float Y(){ return m_fY; }
+  float X()const{ return m_fX; }
+  float Y()const{ return m_fY; }
   
   //getters for returning truncated x and y values
-  int iX(){return (int)m_fX; }
-  int iY(){return (int)m_fY; }
+  int iX()const{return (int)m_fX; }
+  int iY()const{return (int)m_fY; }
 
-  void X(float x){ m_fX = x; }
-  void Y(float y){ m_fY = y; }
+  void X(float x)const{ m_fX = x; }
+  void Y(float y)const{ m_fY = y; }
 
   //arithmetic operations
   
   //addition operations
-  Vec2 operator+(const Vec2& other){ return Vec2(m_fX + other.m_fX, m_fY + other.m_fY); }
+  Vec2 operator+(const Vec2& other)const{ return Vec2(m_fX + other.m_fX, m_fY + other.m_fY); }
   Vec2& operator+=(const Vec2& other){ m_fX += other.m_fX; m_fY += other.m_fY; return *this;}
 
   //subtraction operations
-  Vec2 operator-(const Vec2& other){ return Vec2(m_fX - other.m_fX, m_fY - other.m_fY); }
+  Vec2 operator-(const Vec2& other)const{ return Vec2(m_fX - other.m_fX, m_fY - other.m_fY); }
   Vec2& operator-=(const Vec2& other){ m_fX -= other.m_fX; m_fY -= other.m_fY; return *this;}
 
   //scalar multiplications
-  Vec2 operator*(float val){ return Vec2(m_fX * val, m_fY * val); }
+  Vec2 operator*(float val)const{ return Vec2(m_fX * val, m_fY * val); }
   Vec2 operator*=(float val){ m_fX *= val; m_fY *= val; return *this;}
 
   //vector-vector multiplications
-  Vec2 operator*(const Vec2& other){ return Vec2(m_fX * other.m_fX, m_fY * other.m_fY); }
+  Vec2 operator*(const Vec2& other)const{ return Vec2(m_fX * other.m_fX, m_fY * other.m_fY); }
   Vec2& operator*=(const Vec2& other){ m_fX *= other.m_fX; m_fY *= other.m_fY; return *this; }
 
   //scalar division operations
-  Vec2 operator/(float val){ return Vec2(m_fX / val, m_fY / val); }
+  Vec2 operator/(float val)const{ return Vec2(m_fX / val, m_fY / val); }
   Vec2& operator/=(float val){ m_fX /= val; m_fY /= val; return *this; }
 
   //vector-vector divisions
-  Vec2 operator/(const Vec2& other){ return Vec2(m_fX / other.m_fX, m_fY / other.m_fY); }
+  Vec2 operator/(const Vec2& other)const{ return Vec2(m_fX / other.m_fX, m_fY / other.m_fY); }
   Vec2& operator/=(const Vec2& other){ m_fX /= other.m_fX; m_fY /= other.m_fY; return *this; }
 
   //dot-product method
-  float Dot(const Vec2& other) { return ((m_fX * other.m_fX) + (m_fY * other.m_fY)); }
+  float Dot(const Vec2& other)const { return ((m_fX * other.m_fX) + (m_fY * other.m_fY)); }
   
   //operator overloading for indexing vec2
   float operator[](int index)
@@ -105,6 +106,7 @@ public:
     }
     
   }
+
   
 private:
 
@@ -133,6 +135,61 @@ inline float Norm(const Vec2& v)
 {
   float norm2 = Norm2(v);
   return pow(norm2, 2);
+}
+
+//method for swapping two vec2's
+void Swap(Vec2& v1, Vec2& v2)
+{
+  float tmp = v1.X();
+  v1.X(v2.X());
+  v2.X(tmp);
+
+  tmp = v1.Y();
+  v1.Y(v2.Y());
+  v2.Y(tmp);
+}
+
+//method for printing vec2 for debugging
+std::ostream& operator<<(std::ostream& os, const Vec2& v)const
+{
+  os << "(" << v.X() << ", " << v.Y() << ")";
+  return os;
+}
+
+//---------------------------------Interpolation methods-------------------------------------
+
+std::vector<float> Interpolate(float i0, float d0, float i1, float d1)
+{
+  float epsilon = 0.0001f;
+
+  if(abs(i0 - i1) <= epsilon)
+  {
+    return {d0};
+  }
+    
+  std::vector<float> values;
+
+  float a = (d1 - d0)/(i1 - i0);
+
+  float d = d0;
+
+  for(int i = (int)i0; i <= (int)i1; i++)
+  {
+    values.push_back(d);
+    d = d + a;
+  }
+
+  return values;
+}
+
+std::vector<float> Interpolate(const Vec2& v1, const Vec2& v2)
+{
+  return Interpolate(v1.X(), v1.Y(), v2.X(), v2.Y());
+}
+
+std::vector<float> Interpolate_(const Vec2& v1, const Vec2& v2)
+{
+  return Interpolate(v1.Y(), v1.X(), v2.Y(), v2.X());
 }
 
 #endif
